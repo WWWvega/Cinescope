852c56eaf2af28f6e9cca9335464021f85702124
WWWvega <rezume1779@gmail.com>
2026-03-03 16:15:16 +0500

    Modul7 обновлены тесты с использованием новых фикстур

--- Cinescope_exam/tests/ui/Cinescope/test_login_Cinescope_page.py
+++ Cinescope_exam/tests/ui/Cinescope/test_login_Cinescope_page.py
@@ -8,21 +8,6 @@ from playwright.sync_api import sync_playwright
 @allure.feature("Тестирование Страницы Login")
 @pytest.mark.ui
 class TestloginPage:
-   @allure.title("Проведение успешного входа в систему")
-   def test_login_by_ui(self, registered_user):
-      with sync_playwright() as playwright:
-           browser = playwright.chromium.launch(headless=False)# Запуск браузера headless=False для визуального отображения
-           page = browser.new_page()
-           login_page = CinescopLoginPage(page)# Создаем объект страницы Login
-
-           login_page.open()
-           login_page.login(registered_user.email, registered_user.password) # Осуществяем вход
-
-           login_page.assert_was_redirect_to_home_page() # Проверка редиректа на домашнюю страницу
-           login_page.make_screenshot_and_attach_to_allure() # Прикрепляем скриншот
-           login_page.assert_allert_was_pop_up() # Проверка появления и исчезновения алерта
-
-
    @allure.title("Проверка сообщения об ошибке при вводе неверных данных")
    def test_login_with_invalid_credentials(self, page):
 
